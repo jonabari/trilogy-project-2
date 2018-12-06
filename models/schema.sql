504ce2b81bbb5db55415241b6b9f75668499919c
jonabari <barcelo.jonathan@gmail.com>
2018-12-06 17:57:53 -0600

    plus seeds, some file changes

--- models/schema.sql
+++ models/schema.sql
@@ -1,22 +1,14 @@
 CREATE DATABASE trustApp_db;
 USE trustApp_db;
 
-CREATE TABLE `users`
-(
-  `user_id` Int
-( 11 ) AUTO_INCREMENT NOT NULL,
-  `email` VARCHAR
-( 255) NOT NULL,
-  `password` VARCHAR
-( 255 ) NOT NULL,
-  `names` VARCHAR
-( 255 ) NOT NULL,
-  `lastnames` VARCHAR
-( 255 ) NOT NULL,
-  `qr` TEXT
-( 65535 ) NOT NULL,
-  PRIMARY KEY
-( `user_id` )
+CREATE TABLE users (
+  user_id Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `email` VARCHAR( 255) NOT NULL,
+  `password` VARCHAR( 255 ) NOT NULL,
+  `names` VARCHAR( 255 ) NOT NULL,
+  `lastnames` VARCHAR( 255 ) NOT NULL,
+  `qr` TEXT( 65535 ) NOT NULL,
+  PRIMARY KEY( `user_id` )
 );
 
 CREATE TABLE `orders`
